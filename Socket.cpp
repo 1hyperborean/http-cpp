e3ee34de2254c3b1fcfdeb41e3577575e20f1d49
Nikoloz Beroshvili <nikolozberoshvili22@gmail.com>
2024-11-28 18:48:05 +0400

    init

---
+++ Socket.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nikoloz Beroshvili on 28.11.24.
+//
+
+#include "Socket.h"
---
+++ Socket.h
@@ -0,0 +1,10 @@
+#ifndef WEBSERVER_SOCKET_H
+#define WEBSERVER_SOCKET_H
+
+
+class Socket {
+
+};
+
+
+#endif //WEBSERVER_SOCKET_H
